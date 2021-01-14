afed4a4dd09ec077392a46da11f2bdace7e0f8dd
stdwarf <terrapeFt66>
2021-01-14 19:17:58 +0300

    login form added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -21,3 +22,11 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title = 'Sign in', form=form)
