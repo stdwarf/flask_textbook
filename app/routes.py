5a73aa404970a43978e03cfbfec7a609ebb202db
stdwarf <terrapeFt66>
2021-01-15 18:12:43 +0300

    flask shell enable

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
